124d0400a74c9fb9dc6a8bda006b39deb954f67e
gmy <gmy@1526759495.com>
2023-10-18 20:08:13 +0800

    hot-fix commit2

--- src/main/java/com/mengyuan/git/GitTest.java
+++ src/main/java/com/mengyuan/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello.txt");
         System.out.println("hello.txt");
         System.out.println("gmy");
+        System.out.println("hot-fix test");
     }
 }
 
