42b58884b4703b1be06b7c2731af0ae79f36d786
gmy <gmy@1526759495.com>
2023-10-18 16:58:58 +0800

    second commit

--- src/main/java/com/mengyuan/git/GitTest.java
+++ src/main/java/com/mengyuan/git/GitTest.java
@@ -3,6 +3,7 @@ package com.mengyuan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello.txt");
+        System.out.println("hello.txt");
     }
 }
 
